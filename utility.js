9d39caded20053b6b429f91f3366bff7a617ea57
minkyu <elliotelon@gmail.com>
2024-08-23 10:32:03 +0900

    타입스크립트 유틸리티 - Required

--- utility.js
+++ utility.js
@@ -1,2 +1,25 @@
+// type Partial<T> = {
+//     [P in keyof T]?: T[P]
+// } 
+// type PartialUser = {
+//     name?: string | undefined;
+//     age?: number | undefined;
+//     phone?: number | undefined;
+// }
 var user = { name: "kim" };
-console.log(user.age);
+console.log(user.name); // kim
+console.log(user.age); // undefined
+console.log(user.phone); // undefined
+/*
+type Required_User = {
+    name: string;
+    age: number;
+    phone: number;
+}
+*/
+var user2 = {
+    name: 'lee',
+    age: 22,
+    phone: "01012341234"
+};
+console.log(user2.phone);
--- utility.ts
+++ utility.ts
@@ -23,3 +23,31 @@ let user : PartialUser = {name: "kim"};
 console.log(user.name); // kim
 console.log(user.age); // undefined
 console.log(user.phone); // undefined
+
+
+// type Required<T> = {
+//     [P in keyof T]-?: T[P];
+// };
+
+interface User2 {
+    name?: string;
+    age?: number;
+    phone: string;
+ }
+ 
+ type RequiredUser = Required<User2>; // User의 속성을 모두 일반 타입으로
+ /*
+ type Required_User = {
+     name: string;
+     age: number;
+     phone: number;
+ }
+ */
+ 
+ const user2: Required<User2> = {
+    name: 'lee',
+    age: 22,
+    phone: "01012341234"
+}
+
+console.log(user2.phone)
